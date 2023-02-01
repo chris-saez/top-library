71dcecd1a7489ecf7209f2aadb210cbe5593fc8a
Chris <hello@chris-saez.com>
2023-02-01 00:08:07 -0800

    remove repeating lines inside of the scanBooks function

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 let myLibrary = [
-    {
-        "title": "Harry Potter",
-        "author": "JK Rowling",
-        "pages": "256 Pages",
-        "read": "Read"
-    },
-    {
-        "title": "Derpie Potter",
-        "author": "JK Rowling",
-        "pages": "256 Pages",
-        "read": "Read"
-    }
+    // {
+    //     "title": "Harry Potter",
+    //     "author": "JK Rowling",
+    //     "pages": "256 Pages",
+    //     "read": "Read"
+    // },
+
+    // {
+    //     "title": "Derpie Potter",
+    //     "author": "JK Rowling",
+    //     "pages": "256 Pages",
+    //     "read": "Read"
+    // }
 ];
 
 function Book(title, author, pages) {
@@ -26,12 +27,13 @@ function addBookToLibrary() {
     blackOverlay.classList.remove('hide');
 }
 
-
+// Closes and hides add book form
 function closeForm() {
     bookFormContainer.classList.add('hide');
     blackOverlay.classList.add('hide');
 }
 
+// Creates new book object from user's form inputs, pushes into library array, then renders array of books
 function submitForm(){
     let newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value);
     myLibrary.push(newBook);
@@ -41,99 +43,41 @@ function submitForm(){
 
 // Loops through myLibrary array and renders book objects on to browser
 function scanBooks() {
-    if(librarySection.children.length == 1){
-        console.log("derp");
-        myLibrary.forEach(book =>{ 
-            let libraryCard = document.createElement("div");
-            libraryCard.setAttribute('class', 'book-card');
-            
-            let titleContainer = document.createElement("h1");
-            titleContainer.setAttribute('class', 'book-title-text');
-            let titleText = document.createTextNode(book.title);
-            titleContainer.appendChild(titleText);
-    
-            let authorContainer = document.createElement("p");
-            let authorText = document.createTextNode(book.author);
-            authorContainer.appendChild(authorText);
-    
-            let pagesContainer = document.createElement("p");
-            let pagesText = document.createTextNode(book.pages);
-            pagesContainer.appendChild(pagesText);
-    
-            libraryCard.appendChild(titleContainer);
-            libraryCard.appendChild(authorContainer);
-            libraryCard.appendChild(pagesContainer);
-    
-            addBookButton.before(libraryCard);
-        })
+    let newArray = [];
+
+    if(!bookTitleText.length){
+        newArray = myLibrary;
     } else {
-        
-        // for (i; i<myLibrary.length; i++) {
-        //     console.log("hello-1");
-        //     for(j; j<bookTitleText.length; j++) {
-        //         console.log("hello-2");
-        //         console.log(bookTitleText[j].textContent);
-        //         console.log(myLibrary[i].title);
-        //         if(bookTitleText[j].textContent == myLibrary[i].title){
-        //             continue;
-        //         } else {
-        //                 let libraryCard = document.createElement("div");
-        //                 libraryCard.setAttribute('class', 'book-card');
-                        
-        //                 let titleContainer = document.createElement("h1");
-        //                 titleContainer.setAttribute('class', 'book-title-text');
-        //                 let titleText = document.createTextNode(myLibrary[i].title);
-        //                 titleContainer.appendChild(titleText);
-    
-        //                 let authorContainer = document.createElement("p");
-        //                 let authorText = document.createTextNode(myLibrary[i].author);
-        //                 authorContainer.appendChild(authorText);
-    
-        //                 let pagesContainer = document.createElement("p");
-        //                 let pagesText = document.createTextNode(myLibrary[i].pages);
-        //                 pagesContainer.appendChild(pagesText);
-    
-        //                 libraryCard.appendChild(titleContainer);
-        //                 libraryCard.appendChild(authorContainer);
-        //                 libraryCard.appendChild(pagesContainer);
-    
-        //                 addBookButton.before(libraryCard);
-                        
-        //                 return;
-        //         }
-        //     }
-        // }
-        for (let i=0; i<bookTitleText.length; i++) {
-            if(bookTitleText[i].textContent == myLibrary[i].title){
-                console.log("hit");
-                continue;
-            }
-        }
-        console.log("derpie")
+        // Filter through the existing book objects to store only the newly added one
+        newArray = myLibrary.filter(newBook => {
+            return !Object.values(bookTitleText).some(oldBooks => oldBooks.textContent == newBook.title);
+        });
+    }
+
+    for (let i=0; i<newArray.length; i++) {
         let libraryCard = document.createElement("div");
         libraryCard.setAttribute('class', 'book-card');
         
         let titleContainer = document.createElement("h1");
         titleContainer.setAttribute('class', 'book-title-text');
-        let titleText = document.createTextNode(myLibrary[myLibrary.length-1].title);
+        let titleText = document.createTextNode(newArray[i].title);
         titleContainer.appendChild(titleText);
-
+    
         let authorContainer = document.createElement("p");
-        let authorText = document.createTextNode(myLibrary[myLibrary.length-1].author);
+        let authorText = document.createTextNode(newArray[i].author);
         authorContainer.appendChild(authorText);
-
+    
         let pagesContainer = document.createElement("p");
-        let pagesText = document.createTextNode(myLibrary[myLibrary.length-1].pages);
+        let pagesText = document.createTextNode(newArray[i].pages);
         pagesContainer.appendChild(pagesText);
-
+    
         libraryCard.appendChild(titleContainer);
         libraryCard.appendChild(authorContainer);
         libraryCard.appendChild(pagesContainer);
-
-        addBookButton.before(libraryCard);
-               
-        }
+    
+        addBookButton.before(libraryCard);        
     }
+}
 
 
 
@@ -151,7 +95,3 @@ const bookTitleText = document.getElementsByClassName("book-title-text");
 addBookButton.addEventListener("click", addBookToLibrary);
 confirmBookButton.addEventListener("click", submitForm);
 blackOverlay.addEventListener("click", closeForm);
-
-scanBooks();
-
-// work on adding functionality to the form to add in the book title,author,pages, and have read to the array
